2fcf5e7f35d5697138aa8f0c1bd57f049bcf7023
Maple0922 <maple0922sept@gmail.com>
2021-08-31 20:13:41 +0900

    Update: OoharaScrapingCommand

--- app/Console/Commands/OoharaScrapingCommand.php
+++ app/Console/Commands/OoharaScrapingCommand.php
@@ -3,41 +3,58 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Log;
+use Goutte\Client;
 
 class OoharaScrapingCommand extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
     protected $signature = 'scrape:oohara {url}';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
     protected $description = 'URLからその講義の動画一覧をダウンロードします';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
+    const BASE_URL = "https://goukakuweb3.o-hara.ac.jp";
+
+    const LOGIN_OPTIONS = [
+        "Code" => 6000673,
+        "Login_Password" => 'qqqqqqq1',
+        "Type_System_Site" => 1,
+        "Is_MemoryToLogin" => false
+    ];
+
     public function __construct()
     {
         parent::__construct();
     }
 
-    /**
-     * Execute the console command.
-     *
-     * @return int
-     */
     public function handle()
     {
-        $url = $this->argument('url');
-        echo $url;
+        $accessPageUrl = $this->argument('url');
+        $loginPageUrl = self::BASE_URL;
+        $client = new Client;
+        $loginPage = $client->request('GET', $loginPageUrl);
+
+        // $form = $loginPage->filter('form')->form();
+        $form = $loginPage->selectButton('Login')->form();
+
+        // foreach (self::LOGIN_OPTIONS as $property => $value) {
+        //     $form[$property] = $value;
+        // }
+        // $form['Code'] = 6000673;
+        // $form['Login_Password'] = 'qqqqqqqq1';
+        // $loginButton = $loginPage->selectButton('Login')->form();
+
+        $client->submit($form, self::LOGIN_OPTIONS);
+        $clawler = $client->request('GET', $accessPageUrl);
+        // $client->click($loginButton);
+        // $client->request('POST', $loginPageUrl, self::LOGIN_OPTIONS);
+
+        $response = $clawler->outerHtml();
+        // $response = $clawler->filter('p')->first()->text();
+        // $text = $response->filter('.dv_1')->first()->text();
+        // $text = $response->filter('header > h1 > div')->first()->text();
+
+        Log::info(print_r($response, true));
+        // Log::info(print_r($a, true));
+        return;
     }
 }
